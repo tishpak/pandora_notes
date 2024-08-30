3646e357f0489aa0767b54ee5eadde9666eb65f5
Nickolay <84032953+tishpak@users.noreply.github.com>
2024-08-30 23:05:13 +0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -78,6 +78,7 @@ const config: QuartzConfig = {
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
       Plugin.FolderPage(),
+      Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
